d25c8e4ad95934f9c8baaa941957b4cc83fedad7
Ikey Doherty <ikey.doherty@gmail.com>
2013-12-07 11:17:06 +0000

    Initial creation of budgie-session
    budgie-session will be used to bootstrap the desktop, launching the
    window manager and panel. Eventually it will become a little bit more
    intelligent by relaunching processes. Currently it does nothing.

---
+++ common.mk
@@ -0,0 +1,6 @@
+AM_CFLAGS =  -fstack-protector -Wall -pedantic \
+        -Wstrict-prototypes -Wundef -fno-common \
+        -Werror-implicit-function-declaration \
+        -Wformat -Wformat-security -Werror=format-security \
+        -Wno-conversion -Werror \
+        -DDATADIR=\"$(datadir)\"
