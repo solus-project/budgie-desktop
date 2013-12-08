8fab326fabb5af6f2a5092b7e98745f027facb3f
Ikey Doherty <ikey.doherty@gmail.com>
2013-12-08 00:52:57 +0000

    panel: Stick panel to all desktops

--- panel/budgie-panel.c
+++ panel/budgie-panel.c
@@ -134,6 +134,7 @@ static void budgie_panel_init(BudgiePanel *self)
         /* We want to be a dock */
         gtk_window_set_type_hint(GTK_WINDOW(self),
                 GDK_WINDOW_TYPE_HINT_DOCK);
+        gtk_window_stick(GTK_WINDOW(self));
 
         /* Add a shadow, idea came from wingpanel, kudos guys :) */
         shadow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
