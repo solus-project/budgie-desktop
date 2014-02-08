d5459fc75b802c799d857c7b8f94a43a71ac76ac
Ikey <ikey.doherty@gmail.com>
2014-02-08 14:20:32 +0000

    panel: Implement window_opened wnck method
    We now add a simple GtkToggleButton with a GtkImage to our area when
    an application is opened.

--- panel/icon-tasklist.c
+++ panel/icon-tasklist.c
@@ -20,11 +20,14 @@
  * 
  * 
  */
+#include <libwnck/libwnck.h>
+
 #include "icon-tasklist.h"
 
 /* IconTasklist object */
 struct _IconTasklist {
         GtkBox parent;
+        WnckScreen *screen;
 };
 
 /* IconTasklist class definition */
@@ -39,6 +42,11 @@ static void icon_tasklist_class_init(IconTasklistClass *klass);
 static void icon_tasklist_init(IconTasklist *self);
 static void icon_tasklist_dispose(GObject *object);
 
+/* Private functionality */
+static void window_opened(WnckScreen *screen,
+                          WnckWindow *window,
+                          gpointer userdata);
+
 /* Initialisation */
 static void icon_tasklist_class_init(IconTasklistClass *klass)
 {
@@ -50,12 +58,20 @@ static void icon_tasklist_class_init(IconTasklistClass *klass)
 
 static void icon_tasklist_init(IconTasklist *self)
 {
+        wnck_set_client_type(WNCK_CLIENT_TYPE_PAGER);
+        self->screen = wnck_screen_get_default();
+        g_signal_connect(self->screen, "window-opened",
+                G_CALLBACK(window_opened), self);
+
+        /* Align to the center vertically */
+        gtk_widget_set_valign(GTK_WIDGET(self), GTK_ALIGN_CENTER);
 }
 
 static void icon_tasklist_dispose(GObject *object)
 {
         /* Destruct */
         G_OBJECT_CLASS (icon_tasklist_parent_class)->dispose (object);
+        wnck_shutdown();
 }
 
 /* Utility; return a new IconTasklist */
@@ -63,6 +79,48 @@ GtkWidget *icon_tasklist_new(void)
 {
         IconTasklist *self;
 
-        self = g_object_new(ICON_TASKLIST_TYPE, NULL);
+        self = g_object_new(ICON_TASKLIST_TYPE, NULL, "orientation", GTK_ORIENTATION_HORIZONTAL);
         return GTK_WIDGET(self);
+}
+
+
+static void window_opened(WnckScreen *screen,
+                          WnckWindow *window,
+                          gpointer userdata)
+{
+        GtkWidget *button = NULL;
+        GtkWidget *image = NULL;
+        const gchar *title;
+        const gchar *icon;
+        IconTasklist *self = ICON_TASKLIST(userdata);
+
+        /* Don't add buttons for tasklist skipping apps */
+        if (wnck_window_is_skip_tasklist(window))
+                return;
+
+        title = wnck_window_get_name(window);
+        icon = wnck_window_get_icon_name(window);
+
+        /* Add the image as a primary component */
+        if (!wnck_window_has_icon_name(window))
+                image = gtk_image_new_from_icon_name(icon, GTK_ICON_SIZE_BUTTON);
+        else
+                image = gtk_image_new_from_pixbuf(wnck_window_get_icon(window));
+
+        button = gtk_toggle_button_new();
+        gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
+        gtk_container_add(GTK_CONTAINER(button), image);
+
+        /* Set title as tooltip */
+        gtk_widget_set_tooltip_text(button, title);
+
+        /* Press it if its active */
+        if (wnck_window_is_active(window))
+                gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(button), TRUE);
+
+        /* Store a reference to this window for destroy ops, etc. */
+        g_object_set_data(G_OBJECT(button), "bwindow", window);
+
+        gtk_box_pack_start(GTK_BOX(self), button, FALSE, FALSE, 0);
+        gtk_widget_show_all(button);
 }
