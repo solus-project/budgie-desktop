ab1637c161d3fafd7374bc7e83a229b3b5ab5aa8
Ikey Doherty <ikey@solus-project.com>
2016-07-06 00:44:25 +0100

    Enforce the glib/gtk versioning in CFLAGS for C rewrite
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- common.mk
+++ common.mk
@@ -5,7 +5,12 @@ AM_CFLAGS = \
 	-Wformat -Wformat-security -Werror=format-security 	\
 	-Wconversion -Wunused-variable -Wunreachable-code 	\
 	-Wall -W -D_FORTIFY_SOURCE=2 -std=c11			\
-        -DDATADIR=\"$(datadir)\" -DSYSCONFDIR=\"$(sysconfdir)\" 
+        -DDATADIR=\"$(datadir)\" -DSYSCONFDIR=\"$(sysconfdir)\" \
+	-DGDK_VERSION_MAX_ALLOWED=GDK_VERSION_3_16 		\
+	-DGDK_VERSION_MIN_REQUIRED=GDK_VERSION_3_16		\
+	-DGLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_42		\
+	-DGLIB_VERSION_MIN_REQUIRED=GLIB_VERSION_2_42
+
 
 AM_CPPFLAGS += \
 	-I $(top_srcdir) \
