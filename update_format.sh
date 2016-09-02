9807a53abfebcc108463247d288d3784c2251e9e
Ikey Doherty <ikey@solus-project.com>
2016-09-02 02:25:33 +0100

    Make update_format more useful and include misspell
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- update_format.sh
+++ update_format.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-clang-format -i $(find . -name '*.[ch]' -not -path "./gvc/*" -not -path "./imports/*")
+CI_EXCLUDES="! -path */gvc/* ! -path */imports/natray/*"
+
+# Ensure we're formatted everywhere.
+clang-format -i $(find . $CI_EXCLUDES -name '*.[ch]')
+
+# Check we have no typos.
+which misspell 2>/dev/null >/dev/null
+if [[ $? -eq 0 ]]; then
+    misspell -error `find . $CI_EXCLUDES -name '*.[ch]'`
+    misspell -error `find . $CI_EXCLUDES -name '*.vala'`
+fi
