232228cdecc40b61dad686ca5507fa734b6a4f86
Ikey Doherty <ikey@solus-project.com>
2017-02-22 21:50:50 +0000

    update_format: Limit only to src/
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- update_format.sh
+++ update_format.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Ensure we're formatted everywhere.
-clang-format -i $(find . -name '*.[ch]' -not -path '*/gvc/*' -not -path '*/natray/*')
+clang-format -i $(find src -name '*.[ch]' -not -path '*/gvc/*' -not -path '*/natray/*')
 
 # Check we have no typos.
 which misspell 2>/dev/null >/dev/null
 if [[ $? -eq 0 ]]; then
-    misspell -error `find . -name '*.[ch]' -not -path '*/gvc/*' -not -path '*/natray/*'`
-    misspell -error `find . -name '*.vala' -not -path '*/gvc/*' -not -path '*/natray/*'`
+    misspell -error `find src -name '*.[ch]' -not -path '*/gvc/*' -not -path '*/natray/*'`
+    misspell -error `find src -name '*.vala' -not -path '*/gvc/*' -not -path '*/natray/*'`
 fi
