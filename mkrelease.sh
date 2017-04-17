cfc6131d1524e600b5317095f218d7a174217111
Ikey Doherty <ikey@solus-project.com>
2017-04-18 00:33:55 +0100

    Bump version to 10.3.1
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mkrelease.sh
+++ mkrelease.sh
@@ -5,7 +5,7 @@ git submodule init
 git submodule update
 
 # Script for ikey because he went with meson. *shrug*
-VERSION="10.3"
+VERSION="10.3.1"
 NAME="budgie-desktop"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
