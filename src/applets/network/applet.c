b35687a238a63ef21232b6123406b7aeee4d3de5
Ikey Doherty <ikey@solus-project.com>
2017-07-01 17:20:14 +0100

    applets/network: Improve default applet appearance
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/applets/network/applet.c
+++ src/applets/network/applet.c
@@ -91,10 +91,13 @@ static void budgie_network_applet_init(BudgieNetworkApplet *self)
 
         box = gtk_event_box_new();
         gtk_container_add(GTK_CONTAINER(self), box);
+        g_object_set(box, "halign", GTK_ALIGN_CENTER, "valign", GTK_ALIGN_CENTER, NULL);
 
         /* Default to disconnected icon */
-        image = gtk_image_new_from_icon_name("network-offline-symbolic", GTK_ICON_SIZE_BUTTON);
+        image = gtk_image_new_from_icon_name("network-wired-disconnected-symbolic",
+                                             GTK_ICON_SIZE_INVALID);
         self->image = image;
+        gtk_image_set_pixel_size(GTK_IMAGE(image), 16);
         gtk_container_add(GTK_CONTAINER(box), image);
 
         /* TODO: Hook up signals and popovers and what not */
