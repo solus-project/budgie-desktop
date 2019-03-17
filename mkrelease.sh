38b69f05619f0b1a681276dc258929f2fcb7a750
Joshua Strobl <joshua@stroblindustries.com>
2019-03-17 11:10:16 +0200

    Tag official 10.5

--- mkrelease.sh
+++ mkrelease.sh
@@ -6,7 +6,7 @@ git submodule update
 
 VERSION="10.5"
 NAME="budgie-desktop"
-git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
+git-archive-all --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
 
 gpg --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
