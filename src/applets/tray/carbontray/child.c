07ec8b42b1ef75914eb92225768ab7c13da0dd42
Campbell Jones <git@serebit.com>
2020-08-26 13:00:32 -0400

    Clean up carbontray source and add hint error check

--- src/applets/tray/carbontray/child.c
+++ src/applets/tray/carbontray/child.c
@@ -20,9 +20,8 @@
 
 static void carbon_child_init(CarbonChild*);
 static void carbon_child_realize(GtkWidget*);
-static void carbon_child_get_preferred_width(GtkWidget*, int*, int*);
-static void carbon_child_get_preferred_height(GtkWidget*, int*, int*);
-static void set_wmclass(CarbonChild*, Display*);
+static void carbon_child_get_preferred_size(GtkWidget*, int*, int*);
+static bool set_wmclass(CarbonChild*, Display*);
 
 
 
@@ -68,24 +67,29 @@ CarbonChild* carbon_child_new(int size, GdkScreen* screen, Window iconWindow) {
 		return NULL;
 
 	CarbonChild* self = g_object_new(CARBON_TYPE_CHILD, NULL);
-	self->preferredWidth = size;
-	self->preferredHeight = size;
+	self->preferredSize = size;
 	self->iconWindow = iconWindow;
 	self->isComposited = FALSE;
 	gtk_widget_set_visual(GTK_WIDGET(self), visual);
 
-	/* check if there is an alpha channel in the visual */
-	int red_prec, green_prec, blue_prec;
-	gdk_visual_get_red_pixel_details(visual, NULL, NULL, &red_prec);
-	gdk_visual_get_green_pixel_details(visual, NULL, NULL, &green_prec);
-	gdk_visual_get_blue_pixel_details(visual, NULL, NULL, &blue_prec);
+	int eventBaseReturn, errorBaseReturn; // unused, we only need to know if composite is supported at all
+	bool supportsComposite = XCompositeQueryExtension(xdisplay, &eventBaseReturn, &errorBaseReturn);
 
-	bool supportsComposite = gdk_display_supports_composite(display);
-	if (red_prec + blue_prec + green_prec < gdk_visual_get_depth(visual) && supportsComposite)
-		self->isComposited = TRUE;
+	if (supportsComposite) {
+		// check if there is an alpha channel in the visual. if there is, we can composite it
+		int red_prec, green_prec, blue_prec;
+		gdk_visual_get_red_pixel_details(visual, NULL, NULL, &red_prec);
+		gdk_visual_get_green_pixel_details(visual, NULL, NULL, &green_prec);
+		gdk_visual_get_blue_pixel_details(visual, NULL, NULL, &blue_prec);
+
+		if (red_prec + blue_prec + green_prec < gdk_visual_get_depth(visual))
+			self->isComposited = TRUE;
+	}
 
 	self->wmclass = NULL;
-	set_wmclass(self, xdisplay);
+	if (!set_wmclass(self, xdisplay))
+		// the icon window turned sour while we were getting alpha details. ignore the child
+		return NULL;
 
 	return self;
 }
@@ -95,12 +99,12 @@ void carbon_child_draw_on_tray(CarbonChild* self, GtkWidget* parent, cairo_t* cr
 	g_return_if_fail(parent != NULL);
 	g_return_if_fail(cr != NULL);
 
-	GtkAllocation allocation = {0};
+	GtkAllocation allocation;
 	gtk_widget_get_allocation(GTK_WIDGET(self), &allocation);
 
-	if (!gtk_widget_get_has_window(GTK_WIDGET(parent))) {
-		GtkAllocation parentAllocation = {0};
-		gtk_widget_get_allocation(GTK_WIDGET(parent), &parentAllocation);
+	if (!gtk_widget_get_has_window(parent)) {
+		GtkAllocation parentAllocation;
+		gtk_widget_get_allocation(parent, &parentAllocation);
 
 		allocation.x = allocation.x - parentAllocation.x;
 		allocation.y = allocation.y - parentAllocation.y;
@@ -120,17 +124,14 @@ void carbon_child_draw_on_tray(CarbonChild* self, GtkWidget* parent, cairo_t* cr
 
 static void carbon_child_init(CarbonChild* self) {
 	GtkWidget* widget = GTK_WIDGET(self);
-
 	gtk_widget_set_halign(widget, GTK_ALIGN_CENTER);
 	gtk_widget_set_valign(widget, GTK_ALIGN_CENTER);
-	gtk_widget_set_hexpand(widget, FALSE);
-	gtk_widget_set_vexpand(widget, FALSE);
 }
 
 static void carbon_child_realize(GtkWidget* widget) {
 	CarbonChild* self = CARBON_CHILD(widget);
 
-	gtk_widget_set_size_request(widget, self->preferredWidth, self->preferredHeight);
+	gtk_widget_set_size_request(widget, self->preferredSize, self->preferredSize);
 	GTK_WIDGET_CLASS(carbon_child_parent_class)->realize(widget);
 
 	GdkWindow* window = gtk_widget_get_window(widget);
@@ -148,40 +149,35 @@ static void carbon_child_realize(GtkWidget* widget) {
 	gtk_widget_set_app_paintable(widget, self->parentRelativeBg || self->isComposited);
 }
 
-static void carbon_child_get_preferred_width(GtkWidget* base, int* minimum_width, int* natural_width) {
-	CarbonChild* self = CARBON_CHILD(base);
-	int scale = gtk_widget_get_scale_factor(base);
-
-	*minimum_width = self->preferredWidth / scale;
-	*natural_width = self->preferredWidth / scale;
-}
-
-static void carbon_child_get_preferred_height(GtkWidget* base, int* minimum_height, int* natural_height) {
-	CarbonChild* self = CARBON_CHILD(base);
-	int scale = gtk_widget_get_scale_factor(base);
-
-	*minimum_height = self->preferredHeight / scale;
-	*natural_height = self->preferredHeight / scale;
+static void carbon_child_get_preferred_size(GtkWidget* base, int* minimum_size, int* natural_size) {
+	int preferredSize = CARBON_CHILD(base)->preferredSize;
+	*minimum_size = preferredSize;
+	*natural_size = preferredSize;
 }
 
 static void carbon_child_class_init(CarbonChildClass* klass) {
-	GtkWidgetClass* gtkwidget_class = GTK_WIDGET_CLASS(klass);
+	GtkWidgetClass* widget_class = GTK_WIDGET_CLASS(klass);
 
-	gtkwidget_class->get_preferred_width = carbon_child_get_preferred_width;
-	gtkwidget_class->get_preferred_height = carbon_child_get_preferred_height;
-	gtkwidget_class->realize = carbon_child_realize;
+	widget_class->get_preferred_width = carbon_child_get_preferred_size;
+	widget_class->get_preferred_height = carbon_child_get_preferred_size;
+	widget_class->realize = carbon_child_realize;
 }
 
-static void set_wmclass(CarbonChild* self, Display* xdisplay) {
+static bool set_wmclass(CarbonChild* self, Display* xdisplay) {
 	XClassHint ch;
-	ch.res_class = NULL;
 
 	GdkDisplay* display = gdk_display_get_default();
 	gdk_x11_display_error_trap_push(display);
 	XGetClassHint(xdisplay, self->iconWindow, &ch);
-	gdk_x11_display_error_trap_pop_ignored(display);
+	int error = gdk_x11_display_error_trap_pop(display);
 
-	if (ch.res_class) {
-		self->wmclass = ch.res_class;
+	if (error != 0) {
+		g_warning("Encountered X error %d when obtaining class hint for tray icon", error);
+		return FALSE;
 	}
+
+	if (ch.res_name != NULL) XFree(ch.res_name);
+	if (ch.res_class != NULL) self->wmclass = ch.res_class;
+
+	return TRUE;
 }
